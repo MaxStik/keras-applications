b02f840930cd814ccdf45bb39a4c43aeceb08481
Maxim Stikharev <m@stikharev.ru>
2018-12-12 14:16:08 +0300

    Change link to download

--- keras_applications/vgg19.py
+++ keras_applications/vgg19.py
@@ -19,12 +19,8 @@ from .imagenet_utils import _obtain_input_shape
 
 preprocess_input = imagenet_utils.preprocess_input
 
-WEIGHTS_PATH = ('https://github.com/fchollet/deep-learning-models/'
-                'releases/download/v0.1/'
-                'vgg19_weights_tf_dim_ordering_tf_kernels.h5')
-WEIGHTS_PATH_NO_TOP = ('https://github.com/fchollet/deep-learning-models/'
-                       'releases/download/v0.1/'
-                       'vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5')
+WEIGHTS_PATH = ('http://storage.btc-s.ru/vgg19_weights_tf_dim_ordering_tf_kernels.h5')
+WEIGHTS_PATH_NO_TOP = ('http://storage.btc-s.ru/vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5')
 
 
 def VGG19(include_top=True,
